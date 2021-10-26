4b0f11a6627a4ad150219cf28d1dd68fa99762d7
fontilan <68748054+fontilan@users.noreply.github.com>
2021-10-26 21:40:48 +0200

    add button

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function palindrome(str) {
   let alphanumeric = str.match(/[a-zA-Z0-9]+/g); // match only alphanumeric characters
   if (alphanumeric === null) {
-    return true;  // if after this match nothing remains (there were no alphanumeric characters inside the given string), we are left with a null, which we treat as being a palindrome of itself
-  }
-  else {
-    let firstWord = alphanumeric.join('')  // we join everthing together
-    .toLowerCase()  // and convert to lower case
+    return true; // if after this match nothing remains (there were no alphanumeric characters inside the given string), we are left with a null, which we treat as being a palindrome of itself
+  } else {
+    let firstWord = alphanumeric
+      .join("") // we join everthing together
+      .toLowerCase(); // and convert to lower case
 
     let secondWord = firstWord
-    .split('')  // we split again to individual letters
-    .reverse()  // reverse the string letter by letter
-    .join('')  // join it together again
+      .split("") // we split again to individual letters
+      .reverse() // reverse the string letter by letter
+      .join(""); // join it together again
 
-    if (firstWord === secondWord){
-      return true
-    }
-    else return false
+    if (firstWord === secondWord) {
+      return true;
+    } else return false;
   }
 }
 
-palindrome(annarobiiboranna)
+function palindromeCheck() {}
